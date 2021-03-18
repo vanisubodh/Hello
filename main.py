a9d66af7645993e7a6535550a0b796c5a6244910
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:50:46 +0000

    StaticMethod, Iterable,ClassMethod,MultipleInheritance

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
+Static method
+1. Can a method inside a class be called without creating an object? Yes, you can create a method and call it directly
+2. Why does not everybody like static methods? Static methods go against the paradigm of OOP. So those who like to stick to OOP do not like to use static methods.
+'''
 
-import math
-def modules():
-  print(math.sin(90))
-  newModule.snake()
+'''
+Iterable
+1. What is an iterable? An iterable is any object that can be iterated over (you can go one by one through)
+2. Which types of data can be used with an iterable? You can store any type of object in an Iterable
+'''
 
+'''
+Classmethod
+1. What is a classmethod? A class method is shared amongst the different objects in an application
+2. How does a classmethod differ from a staticmethod? class methods are called either from instances of the class, or from the class itself, where static methods are juist called from the class itself.
+'''
 
-  #1. Import the math module and call the sin function
-#2. Create your own module with the function snake()
-
-class App(object):
-  def start(self):
-    print('Starting')
-
-class DisApp(object):
-  def start(self):
-    print("Starting")
-
-class Apple(App):
-  def getVersion(self):
-    print('Apple version')
-
-class Linux(App, DisApp):
-  def getLinuxVersion(self):
-    print('Linux version')
-
-    #Inheritance
-#1. Create a new class that inherits from the class App
-#2. Try to create a class that inherits from two super classes (multiple inheritance)
+'''
+Multiple inheritance
+1. Do all programming languages support multiple inheritance? No, Java for example does not support multiple inheritance
+2. Why would you not use multiple inheritance? Multiple inheritance is dangerous because if you have a method shared across two classes, it can cause a conflict.
+3. Is there a limit to the number of classes you can inherit from? There is no limit to the number of classes you can inherit from
+'''
