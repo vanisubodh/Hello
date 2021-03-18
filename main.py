9fb9afbe369fa6ee5d72e9a827b03bf725f46999
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:42:28 +0000

    CLASS--1. Can you have more than one class in a file? Yes, it is possible to have multiple classes in a file 2. Can multiple objects be created from the same class? Yes, a class can create multiple objects within it 3. Can objects create classes? No, they cannot 4. Using the code above, create another object 5. Add a method to the class: location()

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+class cat:
 
-import datetime
-def timeAndDate():
+  def __init__(self, breed):
+    self.breed = breed
+    self.age = 1
 
-  dateToday = datetime.datetime.now()
-  print("Today is %d-%d-%d" % (dateToday.year, dateToday.month, dateToday.day))
-timeAndDate()
+  def location():
+    x, y = 50
+    return x, y
+
+  def getAge():
+    return age
+  def setAge(self, age):
+    self.age = age
+
+
+
+#'''
+#Getter and setter
+#1. Add a variable age and create a getter and setter (added #this to the class above since it made the most sense)
+#2. Why would you use getter and setter methods? Controlled #access, with getters and setters, you can control how others #access variables in classes
+#'''
