d40d3888fd2d53561e00ee91d61f6ec1685673cf
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:23:29 +0000

    Dictionary 1. Make a mapping from countries to country short codes 2. Print each item (key and value)

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-def rangeFunction():
+def dictionary():
+  countries = {'United States of America' : 'USA', 'Germany' : 'GER', 'France' : 'FRA', 'United Kingdom' : 'UK'}
 
-  thousand = range(1, 1000)
+  for country in countries:
+    print("%s : %s" % (country, countries[country]))
 
-  min = 1000
-  max = 0
-
-  for element in thousand:
-    if element < min:
-      min = element
-      continue
-    elif element > max:
-      max = element
-      continue
-
-  print("max: %d" % (max))
-  print("min: %d" % (min))
-  
-  even = []
-  odd = []
-
-  for element in range(0, 11, 2):
-    even.append(element)
-
-  for element in range(1, 11, 2):
-    odd.append(element)
-
-  print(even)
-  print(odd)
-
-rangeFunction()
+dictionary()
