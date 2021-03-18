1d0728f90c7dce185eea9eceb0ee3d6c39ccc6d7
vanisubodh <vanisubodh@gmail.com>
2021-03-18 13:43:54 +0000

    Make a program that asks a phone number. 2. Make a program that asks the users preferred programming language. '''

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
-#Printin random numbers in range
+#Keyboard input
 
-import random
+def keyboardInput():
+  phone = input("Please Enter Your Phone Number: ")
+  print("Your number is %s" % (phone))
 
-def randomNumbers():
-  x = random.randint(0, 10)
+  language = input("Plese Enter Your preferred programming language: ")
+  print("Your prefered programming language is %s" % (language))
 
-  print(x)
-
-  print("")
-
-  i = 0
-
-  while(i<3):
-
-    print(random.randint(0, 100))
-
-    i += 1
-
-  i = 0
-
-  print("")
-
-  numberOccurances = {}
-
-  while (i < 100):
-
-    rand = random.randint(0,100)
-
-    if(rand not in numberOccurances):
-      numberOccurances[rand] = 1
-    else:
-      numberOccurances[rand] += 1
-
-    i += 1
-
-  print(numberOccurances)
-randomNumbers()
