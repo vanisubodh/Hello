a17c2721c1c80e9385cd7cb2e725f822cdda8455
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:05:15 +0000

    While loops 1. Make a program that lists the countries in the set below using a while loop. clist = ["Canada","USA","Mexico"] 2. What‟s the difference between a while loop and a for loop? A for loop has its iterative value inside the call of it, a while loop only has the condition it will exit on when called. 3. Can you sum numbers in a while loop? Yes 4. Can a for loop be used inside a while loop? Yes, this is called a nested loop

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-def forLoops():
-  clist = ['Canada','USA','Mexico','Australia']
+def whileLoops():
+  clist = ["Canada","USA","Mexico"]
+  i = 0
 
-  for listing in clist:
-    print(listing)
-
-  for num in range(0,100):
-    print(num, end=' ')
-
-  print("")
-
-  for mult1 in range(1,13):
-    for mult2 in range(1, 13):
-      print(mult1 * mult2, end=' ')
-
-    print("")
-
-  for num in range(10, 1):
-    print(num, end=' ')
+  while(i < len(clist)):
+    print(clist[i], end=' ')
+    i += 1
 
   print("")
 
-  for num in range(0, 10):
-    if(num % 2 == 0):
-      print(num, end=' ')
-
-  print("")
-
+  i = 1
   summation = 0
-  for num in range(100, 200):
-    summation += num
 
+  while(i in range(1,11)):
+    summation += i
+    i += 1
   print(summation)
-forLoops()
+whileLoops()
