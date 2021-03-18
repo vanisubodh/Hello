3a63a9e14c2fa8c4defd1ac196dd2924e7b2f6a2
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:29:16 +0000

    Nested loops 1. Given a tic-tac-toe board of 3x3, print every position 2. Create a program where every person meets the other persons = [ “John”, “Marissa”, “Pete”, “Dayton” ] 3. If a normal for loop finishes in n steps O(n), how many steps has a nested loop? No, generally, for a nested loop, it finishes in O(n^2) for 2 loops, and increasing for each new loop '''

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def writeFile():
-  file = open("txt.txt", "a")
+def nestedLoops():
+  for i in range(1, 4):
+    print("[", end=' ')
+    for j in range (1, 4):
+      print("_", end=' ')
+    print("]")
 
-  file.write("Take it easy")
-  file.write("open(\"text.txt\")")
-writeFile()
+  persons = ["John", "Marissa", "Pete", "Dayton"]
+
+  for person1 in persons:
+    for person2 in persons:
+      print(person1 + " -> " + person2)
+nestedLoops()
