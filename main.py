c91d0f4d254f593a3fc2c43d19047f958a90e2d0
vanisubodh <vanisubodh@gmail.com>
2021-03-18 14:03:16 +0000

    1. Make a program that lists the countries in the set clist = ['Canada','USA','Mexico','Australia'] 2. Create a loop that counts from 0 to 100 3. Make a multiplication table using a loop 4. Output the numbers 1 to 10 backwards using a loop 5. Create a loop that counts all even numbers to 10 6. Create a loop that sums the numbers from 100 to 200

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from getpass import getpass
+def forLoops():
+  clist = ['Canada','USA','Mexico','Australia']
 
-password = getpass()
-if(password != ""):
-    print("Received a password")
-else:
-    print("Did not receive a password")
-     
+  for listing in clist:
+    print(listing)
+
+  for num in range(0,100):
+    print(num, end=' ')
+
+  print("")
+
+  for mult1 in range(1,13):
+    for mult2 in range(1, 13):
+      print(mult1 * mult2, end=' ')
+
+    print("")
+
+  for num in range(10, 1):
+    print(num, end=' ')
+
+  print("")
+
+  for num in range(0, 10):
+    if(num % 2 == 0):
+      print(num, end=' ')
+
+  print("")
+
+  summation = 0
+  for num in range(100, 200):
+    summation += num
+
+  print(summation)
+forLoops()
